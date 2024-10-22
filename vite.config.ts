f4f956bc28d4eff0b5e81fe3c4b78cda56d92093
Jonathan Alvarez <jalvarez@axientcorp.com>
2024-10-22 19:17:11 +0000

    vite resium plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import cesium from 'vite-plugin-cesium';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cesium()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   define: {
-    CESIUM_BASE_URL: JSON.stringify('/cesium'),  // This now points to public/cesium/
+    CESIUM_BASE_URL: JSON.stringify('/cesium'),
   },
 });
